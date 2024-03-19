ced01ea640bf5ac0241d2061f58c7926c01d4dd6
joker <jocker.ss.cat@gmail.com>
2024-03-19 12:33:58 +0800

    first commit

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# 錯誤終止腳本
+set -e
+
+# 運行 gulp build
+npm run build
+
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/joker-cat/vue2023-deploy.git master:gh-pages
+
+cd -
